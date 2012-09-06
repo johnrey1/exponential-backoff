a6eb41354f96b33462072a3cc57cd7a83b06ab44
John Reynolds <john@thejohnreynolds.com>
2012-09-05 23:23:57 -0700

    First draft of exponential backoff. defaults cap at 10

---
+++ Backoff.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ExponentialBackoff
+{
+    using System;
+
+    public class Backoff
+    {
+
+        /// <summary>
+        /// compute the backoff
+        /// throws an exception if C &lteq; 0
+        /// 
+        /// </summary>
+        /// <param name="c">the number sequential collisions (or events) that have occurred warranting backoff</param>
+        /// <param name="interval">the base time interval to use to compute backoff in the case of c = 1</param>
+        /// <returns></returns>
+        public static long GetBackoff(int c, long interval, int ceiling = 10)
+        {
+            if (c <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Pass in a number greater than 0");
+            }
+
+            int max_k = (int)Math.Pow(2,Math.Min(c,ceiling)) - 1;
+            
+            Random r = new Random();
+            int k = r.Next(max_k);
+
+            return k * interval;
+        }
+    }
+}
---
+++ ExponentialBackoff.UnitTest/BackoffTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExponentialBackoff.UnitTest
+{
+    [TestClass]
+    public class BackoffTest
+    {
+        private readonly int TestIterations = 10000;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void CNegativeTest()
+        {
+            Backoff.GetBackoff(-1, 1000);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void CZeroTest()
+        {
+            long backoff = Backoff.GetBackoff(0, 1000);
+        }
+
+        /// <summary>
+        /// verify the case where c = 0
+        /// </summary>
+        [TestMethod]
+        public void C1Test()
+        {
+            for (int i = 0; i < TestIterations; i++)
+            {
+                long backOff = Backoff.GetBackoff(1, 1);
+                Assert.IsTrue(backOff == 0 || backOff == 1, "Backoff = " + backOff);
+            }
+        }
+
+        /// <summary>
+        /// verify the case where c = 0
+        /// </summary>
+        [TestMethod]
+        public void C3Test()
+        {
+            for (int i = 0; i < TestIterations; i++)
+            {
+                long backOff = Backoff.GetBackoff(2, 1);
+                Assert.IsTrue(backOff == 0 || backOff == 1 || backOff == 7, "Backoff = " + backOff);
+            }
+        }
+
+        /// <summary>
+        /// verify the case where c = 0
+        /// </summary>
+        [TestMethod]
+        public void CCeilingTest()
+        {
+            int c = 100;
+
+            for (int i = 0; i < TestIterations; i++)
+            {
+                Assert.IsTrue(Backoff.GetBackoff(c, 1) <= 2047);
+            }
+        }
+
+    }
+}
